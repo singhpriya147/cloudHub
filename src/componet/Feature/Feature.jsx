5b3d64f68787f15040f4c6cd6ea91baa824f676d
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2024-12-11 10:13:27 +0530

    modified feedback section

--- src/componet/Feature/Feature.jsx
+++ src/componet/Feature/Feature.jsx
@@ -1,13 +1,12 @@
-import person1 from '../../assets/persons/person01.png';
-import person2 from '../../assets/persons/person2.png';
-import person3 from '../../assets/persons/person3.png';
-import AddIcon from '../../assets/addIcon.svg';
+
+import codeImage from '../../assets/codeBlock.svg';
+import feedbackImg from '../../assets/FeedBackCard.svg';
 import './Feature.css'; 
 
 const Feature = () => {
   return (
     <div className='container'>
-      {/* Share Section */}
+     
       <div className='share-section'>
         <div className='share-section-left'>
           <sapn className='badge'>Share</sapn>
@@ -26,28 +25,18 @@ const Feature = () => {
           </a>
         </div>
         <div className='code-block-container'>
+         
           <div style={{ backgroundColor: '#FAFAFA', borderRadius: '20px' }}>
-            <div className='code-block'>
-              <pre>
-                <code>
-                  {`no ApiCheck('hello-api-1', {
-  name: 'Hello API',
-  activated: true,
-  request: {
-    method: 'GET',
-    url: 'https://api.checklyhq.com/public-url',
-  assertions: [
-    assertionBuilder.statusCode().equals(200)
-  ]
-});`}
-                </code>
-              </pre>
-            </div>
+            <img
+              src={codeImage}
+              alt=''
+              style={{ borderRadius: '25px', height: '100%', width: '100%' ,objectFit:'contain'}}
+            />
           </div>
         </div>
       </div>
 
-      {/* Feedback Section */}
+   
       <div className='feedback-section'>
         <div className='feedback-section-left'>
           <sapn className='badge'>Feedback</sapn>
@@ -61,64 +50,8 @@ const Feature = () => {
             See how it works
           </a>
         </div>
-        <div style={{ backgroundColor: '#FAFAFA',borderRadius:'20px' }}>
-          <div className='feedback-list'>
-            <div>
-              <h4>Looking for Feedback?</h4>
-            </div>
-
-            <div className='feedback-item'>
-              <img src={person1} alt='' style={{ borderRadius: '100%' }} />
-              <span>Buzz Ubermorn</span>
-              <span className='status'>Pending</span>
-            </div>
-            <hr
-              style={{
-                height: '0.2px',
-                backgroundColor: 'grey',
-                opacity: '50%',
-                borderRadius: '2px',
-              }}
-            />
-            <div className='feedback-item'>
-              <img src={person2} alt='' />
-              <span>Gabriel Valdivia</span>
-              <span className='status'>Done</span>
-            </div>
-            <hr
-              style={{
-                height: '0.2px',
-                backgroundColor: 'grey',
-                opacity: '50%',
-                borderRadius: '2px',
-              }}
-            />
-            <div className='feedback-item'>
-              <img src={person3} alt='' />
-              <span>Jochen Dietz</span>
-              <span className='status'>Requested</span>
-            </div>
-            <hr
-              style={{
-                height: '0.2px',
-                backgroundColor: 'grey',
-                opacity: '50%',
-                borderRadius: '2px',
-              }}
-            />
-            <div
-              style={{
-                color: '#FD4E26',
-                display: 'flex',
-                alignItems: 'center',
-                gap:'1rem'
-              }}
-            >
-              {' '}
-              <img src={AddIcon} alt='' />
-              Add More
-            </div>
-          </div>
+        <div style={{ backgroundColor: '#FAFAFA', borderRadius: '20px' }}>
+          <img src={feedbackImg} alt='' style={{height:'100%', width:'100%',objectFit:'contain'}} />
         </div>
       </div>
     </div>
--- src/componet/Progress/Progress.jsx
+++ src/componet/Progress/Progress.jsx
@@ -1,17 +1,23 @@
 import './Progress.css'
+import projectImg from '../../assets/projectImage.svg';
+import profileImg from '../../assets/ProfileImage.svg';
 
 const Progress = () => {
   return (
     <section className='progress-wrapper'>
       <div className='progress-header'>
-        <h1>Your Archive of progress</h1>
+        <h2>Your Archive of progress</h2>
       </div>
       <main className='progress-main'>
-        <div>
-
+        <div className='card'>
+          <span>Projects</span>
+          <p>Organize, follow, and archive.</p>
+          <img src={projectImg} alt='' />
         </div>
-        <div>
-
+        <div className='card'>
+          <span>Member profiles</span>
+          <p>Automatic portfolios of your work.</p>
+          <img src={profileImg} alt='' />
         </div>
       </main>
     </section>
